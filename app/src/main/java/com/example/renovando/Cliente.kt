140e5ef32f08d9742fefdc295d6df5889354b730
tetano <viniciusvicco@gmail.com>
2019-05-28 11:44:30 -0300

    Estudando para algumas aplicações

---
+++ app/src/main/java/com/example/renovando/Cliente.kt
@@ -0,0 +1,10 @@
+package com.example.tutorialrecycleview
+
+data class Cliente (var nome: String, var peso: Double, var altura: Double, var login: String, var senha: String, var status : Boolean,
+var planorestante: Int) {
+
+
+
+}
+
+
